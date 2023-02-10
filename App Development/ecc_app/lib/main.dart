c6a1d6863411f92e5b374f538b173a8da0f322ea
Neev Jain <neev.jain2122@xaviers.edu.in>
2023-02-10 08:35:46 +0530

    commit
    Signed-off-by: Neev Jain <neev.jain2122@xaviers.edu.in>

--- App Development/ecc_app/lib/main.dart
+++ App Development/ecc_app/lib/main.dart
@@ -2,11 +2,8 @@ import 'package:ecc_app/pages/login.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-
  void main() {
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
---
+++ App Development/practice/practice/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:practice/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage()
+
+      );
+    
+  }
+}
